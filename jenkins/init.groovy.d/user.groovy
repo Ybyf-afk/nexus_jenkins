537a4ac782ad9280c7a4e2afdc375ea096ae90b9
Ybyf-afk <ns-box@mail.ru>
2020-03-10 21:45:45 +0300

    next commit

--- jenkins/init.groovy.d/user.groovy
+++
@@ -1,6 +0,0 @@
-def githubAccount = new UsernamePasswordCredentialsImpl(
-        CredentialsScope.GLOBAL, "admin-nexus", "Test Nexus Account",
-        "admin_nina",
-        "Ninasv31"
-)
-store.addCredentials(domain, nexusAccount)
